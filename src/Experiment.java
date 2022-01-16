491e79f574e9ce25c8e60cce6783a7c347179bb0
sanjiv576 <83968516+sanjiv576@users.noreply.github.com>
2022-01-16 16:18:55 +0545

    Testing file

--- src/Experiment.java
+++ src/Experiment.java
@@ -12,9 +12,29 @@ class Experiment{
 //    }
 
     public static void main(String[] args) {
-        String districts[] = {"Ramechhap", "Kathmandu", "Sindhuli", "Chitwan", "Pokhara", "Jhapa"};
-        for (int i = 0; i < districts.length; i++){
-            System.out.println(districts[i]);
+//        String districts[] = {"Ramechhap", "Kathmandu", "Sindhuli", "Chitwan", "Pokhara", "Jhapa"};
+//        for (int i = 0; i < districts.length; i++){
+//            System.out.println(districts[i]);
+//        }
+
+
+        String[] allFields = {"Sanjiv", "", "Shrestha", "2323", "", "shrestha@"};
+        String[] fieldsName = {"newFirstName", "newMiddleName", "newLastName", "newContact", "newAddress", "newEmail"};
+
+
+        String emptyFields = "";
+        String filledFields = "";
+        for (int i = 0; i < allFields.length; i++){
+            if (allFields[i].isEmpty()){
+                emptyFields += i;
+                System.out.println(fieldsName[i] + " : ");
+                continue;
+            }
+            filledFields += i;
+            System.out.println(fieldsName[i] + " : " + allFields[i]);
         }
+
+        System.out.println("empty fields index : " + emptyFields);
+        System.out.println("Filled fields index : " + filledFields);
     }
 }
