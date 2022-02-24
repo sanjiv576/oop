09c41e63ed5042d950c555967e5e7c0d6f88824f
sanjiv576 <83968516+sanjiv576@users.noreply.github.com>
2022-02-24 22:12:16 +0545

    removed bug from sorting and searching

--- src/main/java/searching/BinarySearch.java
+++ src/main/java/searching/BinarySearch.java
@@ -4,23 +4,23 @@ public class BinarySearch implements Searching, Sorting{
 
 
     @Override
-    public int[] sort(int[] array) {
+    public int[] sort(int[] arr) {
 
+        int temp;
         boolean swap = true;
         while (swap){
             swap = false;
-            for (int i = 0; i<array.length-1; i++){
-                if (array[i] > array[i+1]){
+            for (int i = 0; i<arr.length-1; i++){
+                if (arr[i] > arr[i+1]){
                     // now swapping the elements
-                    array[i] = array[i+1];
-                    array[i+1] = array[i];
+                    temp = arr[i];
+                    arr[i] = arr[i+1];
+                    arr[i+1] = temp;
                     swap = true;
                 }
             }
         }
-
-        System.out.println(array);
-        return array;
+        return arr;
     }
 
     @Override
